505abea894e22cc1d33bb170a55e232aaea189e0
i-Tutu <ignatiustutu@gmail.com>
2021-11-24 17:06:14 +0000

    Copier file

---
+++ tututrader.py
@@ -0,0 +1,14 @@
+print("Hello Trader")
+
+# from telegram.client import Telegram
+
+# tg = Telegram(
+
+# 	api_id = 'api_id',
+# 	api_hash = 'api_hash',
+# 	phone = '+233262828395',
+# 	database_encryption_key = '',
+
+# 	) 
+
+# tg.login()
